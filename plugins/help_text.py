824165cf5e0068ace72ff201fa89b2ccc749b168
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-07-06 13:32:47 +0530

    help and start commands

---
+++ plugins/help_text.py
@@ -0,0 +1,41 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyrogram import Client, filters
+
+    
+@Client.on_message(filters.command("help"))
+async def help_user(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+
+@Client.on_message(filters.command("start"))
+async def start(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        reply_to_message_id=update.message_id
+    )
+ 
