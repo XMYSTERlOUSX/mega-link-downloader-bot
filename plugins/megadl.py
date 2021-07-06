43980cbc213a9538f7b9bd03ae29644988a38f05
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-07-06 15:47:02 +0530

    Everything Happens in Here!

--- plugins/megadl.py
+++ plugins/megadl.py
@@ -304,7 +304,7 @@ async def progress_for_pyrogram(
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        bot_nmae = Config.Bot_username
+        bot_name = Config.Bot_username
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
