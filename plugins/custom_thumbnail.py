93df80cc721cdfb0e315121744c74361157ba4c3
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-07-06 13:46:03 +0530

    For adding custom-permanent thumbnail.

---
+++ plugins/custom_thumbnail.py
@@ -0,0 +1,64 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import numpy
+import os
+from PIL import Image
+import time
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+from pyrogram import Client, filters
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@Client.on_message(filters.photo)
+async def save_photo(bot, update):
+    if update.media_group_id is not None:
+        # album is sent
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+        # create download directory, if not exist
+        if not os.path.isdir(download_location):
+            os.makedirs(download_location)
+        await bot.download_media(
+            message=update,
+            file_name=download_location
+        )
+    else:
+        # received single photo
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        await bot.download_media(
+            message=update,
+            file_name=download_location
+        )
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+            reply_to_message_id=update.message_id
+        )
+
+        
+@Client.on_message(filters.command(["deletethumbnail"]))
+async def delete_thumbnail(bot, update):
+    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+    try:
+        os.remove(download_location + ".jpg")
+        # os.remove(download_location + ".json")
+    except:
+        pass
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
+        reply_to_message_id=update.message_id
+    )
