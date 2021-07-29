a3f30d5f30b4805304c45e85dc2948bc71fe3ceb
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-07-29 11:17:05 +0530

    V2 - Fixed All Issues!

--- sample_config.py
+++ sample_config.py
@@ -16,14 +16,6 @@ class Config(object):
     Mega_password = os.environ.get("Mega_password", "") # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
     #If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
     
-    #Make a channel and add your bot as an admin to it. This channel is for logging the details when processes are started and finished.
-    #Input the channel's id here.👇 If you don't know how to get the channel id send a message in the channel and forward it to https://t.me/AH_ForwarderBot and copy the chat id and enter it in the below field (must include -100 in front)
-    Log_channel_id = os.environ.get("Log_channel_id", "")
-    #If deploying on vps edit the above value as example := Log_channel_id = "Your-Log_channel_id-inside-inverted-commas."
-    
-    Log_channel_username = os.environ.get("Log_channel_username", "") # The username of the log channel you created above! (must enter with '@' in the front of the username)
-    #If deploying on vps edit the above value as example := Log_channel_username = "Your-Log_channel_username-inside-inverted-commas."
-    
     Bot_username = os.environ.get("Bot_username", "") # Your bot's telegram username (must enter with '@' in the front of the username)
     #If deploying on vps edit the above value as example := Bot_username = "Your-Bot_username-inside-inverted-commas."
     
