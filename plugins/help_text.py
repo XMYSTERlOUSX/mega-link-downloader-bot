f1c28a3810acc6e29f3b0e4026d614ce20ece875
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-07-29 11:23:45 +0530

    V2 - Fixed All Issues!

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -29,10 +29,12 @@ async def help_user(bot, update):
         reply_to_message_id=update.message_id
     )
 
+    
 @Client.on_message(filters.command("start"))
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
+        disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
