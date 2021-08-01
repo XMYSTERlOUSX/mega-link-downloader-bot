66adc05018bb166d7bff0d438a1a8ffdb97c7554
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-01 19:20:09 +0530

    Create __init__.py
    Added Database!

---
+++ database/__init__.py
@@ -0,0 +1,26 @@
+import ast
+import redis
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+INFO = Config.REDIS_URI.split(":")
+
+DB = redis.StrictRedis(
+    host=INFO[0],
+    port=INFO[1],
+    password=Config.REDIS_PASS,
+    charset="utf-8",
+    decode_responses=True,
+)
+
+def get_stuff(WHAT):
+    n = []
+    cha = DB.get(WHAT)
+    if not cha:
+        cha = "{}"
+    n.append(ast.literal_eval(cha))
+    return n[0]
