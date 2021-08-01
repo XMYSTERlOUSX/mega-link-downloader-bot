2e38ed3aa8e235dcc5c4658eacf668009aa8d770
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-01 19:36:50 +0530

    For blacklisting unwanted users from using the bot

---
+++ plugins/blacklist.py
@@ -0,0 +1,39 @@
+from pyrogram import Client, filters
+
+from database.blacklist import add_blacklist, get_blacklisted, remove_blacklist
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+  
+
+@Client.on_message(filters.command("black") & filters.user(int(Config.OWNER_ID)))
+async def black_user(_, message):
+    try:
+        bl = int(message.text.split(" ", maxsplit=1)[1])
+    except IndexError:
+        return await message.reply_text("Whom to Blacklist ?")
+    add_blacklist(bl)
+    await message.reply_text(f"Blacklisted {bl} !")
+
+
+@Client.on_message(filters.command("unblack") & filters.user(int(Config.OWNER_ID)))
+async def unblack_user(_, message):
+    try:
+        bl = int(message.text.split(" ", maxsplit=1)[1])
+    except IndexError:
+        return await message.reply_text("Whom to Blacklist ?")
+    te = remove_blacklist(bl)
+    await message.reply_text(te)
+
+
+@Client.on_message(filters.command("listblack") & filters.user(int(Config.OWNER_ID)))
+async def liblack(_, message):
+    m = await message.reply_text("`...`")
+    al = get_blacklisted()
+    TE = "List of Blacklisted User !"
+    for on in al:
+        TE += "\n" + str(on)
+    await m.edit_text(TE)
