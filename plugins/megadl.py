53c32b8729fa2d39ca24ade19726f300899aec0a
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-01 18:47:55 +0530

    Update megadl.py

--- plugins/megadl.py
+++ plugins/megadl.py
@@ -291,12 +291,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n○ <b>𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲 :</b> {2}%\n○ <b>𝗖𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 :</b> ".format(
+        progress = "[{0}{1}] \n○ <b>Percentage ⚡️ :</b> {2}%\n○ <b>Finished ✅ :</b> ".format(
             ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n○ <b>𝗦𝗽𝗲𝗲𝗱 :</b> {2}/s\n○ <b>𝗧𝗶𝗺𝗲 𝗟𝗲𝗳𝘁 :</b> {3}\n\n<b>uploading by {4} </b>\n".format(
+        tmp = progress + "{0} of {1}\n○ <b>Speed 🚀 :</b> {2}/s\n○ <b>Time left 🌝 :</b> {3}\n\n<b>uploading by {4}</b>\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
