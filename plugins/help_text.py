5bfd5919b7d07d14fdb94a67b269be0e57301df9
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-03 09:47:49 +0530

    V2

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -5,13 +5,12 @@ logger = logging.getLogger(__name__)
 
 import os
 
-# the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-# the Strings used for this "thing"
+
 from translation import Translation
 
 import pyrogram
