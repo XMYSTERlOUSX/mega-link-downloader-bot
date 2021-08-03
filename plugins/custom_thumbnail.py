d9e415764d80ff653b919bee45d8b37c2d6805c1
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-03 09:48:18 +0530

    V2

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -8,13 +8,11 @@ import os
 from PIL import Image
 import time
 
-# the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-# the Strings used for this "thing"
 from translation import Translation
 
 import pyrogram
