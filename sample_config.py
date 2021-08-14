9422ef5ab906e26afce306e55eec31a322500542
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-14 22:14:50 +0530

    V3

--- sample_config.py
+++ sample_config.py
@@ -10,10 +10,10 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH") # Get this value from https://my.telegram.org/apps
     #If deploying on vps edit the above value as example := API_HASH = "Your-API_HASH-inside-inverted-commas."
     
-    Mega_email = os.environ.get("Mega_email") # This is not necessary! Enter your mega email only if you have a mega.nz account with pro/business features.
+    Mega_email = os.environ.get("Mega_email", None) # This is not necessary! Enter your mega email only if you have a mega.nz account with pro/business features.
     #If deploying on vps edit the above value as example := Mega_email = "Your-Mega_email-inside-inverted-commas."
     
-    Mega_password = os.environ.get("Mega_password") # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
+    Mega_password = os.environ.get("Mega_password", None) # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
     #If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
     
     Bot_username = os.environ.get("Bot_username", "") # Your bot's telegram username (must enter with '@' in the front of the username)
