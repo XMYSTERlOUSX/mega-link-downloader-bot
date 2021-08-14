f7b5c7522a3eb53b0e7aba3629bd6d8178005926
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-14 20:35:59 +0530

    V3

--- sample_config.py
+++ sample_config.py
@@ -10,10 +10,10 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH") # Get this value from https://my.telegram.org/apps
     #If deploying on vps edit the above value as example := API_HASH = "Your-API_HASH-inside-inverted-commas."
     
-    Mega_email = os.environ.get("Mega_email", "") # This is not necessary! Enter your mega email only if you have a mega.nz account with pro/business features.
+    Mega_email = os.environ.get("Mega_email", None) # This is not necessary! Enter your mega email only if you have a mega.nz account with pro/business features.
     #If deploying on vps edit the above value as example := Mega_email = "Your-Mega_email-inside-inverted-commas."
     
-    Mega_password = os.environ.get("Mega_password", "") # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
+    Mega_password = os.environ.get("Mega_password", None) # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
     #If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
     
     Bot_username = os.environ.get("Bot_username", "") # Your bot's telegram username (must enter with '@' in the front of the username)
@@ -22,10 +22,10 @@ class Config(object):
     OWNER_ID = os.environ.get("OWNER_ID", None) # Your(owner's) telegram id
     #If deploying on vps edit the above value as example := OWNER_ID = Your-telegram id-without-inverted-commas
     
-    REDIS_URI = os.environ.get("REDIS_URI", None) # Get This Value from https://app.redislabs.com/#/login
+    REDIS_URI = os.environ.get("REDIS_URI", None) # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
     #If deploying on vps edit the above value as example := REDIS_URI = "Your-Redis-Endpoint-inside-inverted-commas."
     
-    REDIS_PASS = os.environ.get("REDIS_PASS", None) # Get This Value from https://app.redislabs.com/#/login
+    REDIS_PASS = os.environ.get("REDIS_PASS", None) # Get This Value from http://redislabs.com/try-free (If you don't know how to obtain the a video tutorial is available here:- https://t.me/botzupdate/5)
     #If deploying on vps edit the above value as example := REDIS_PASS = "Your-Redis-Password-inside-inverted-commas."
 
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split()) # Id's of the telegram users, who you want to allow for multitasking - downloading multiple links at once!
@@ -34,5 +34,7 @@ class Config(object):
     
     #If deploying on vps edit the above value as example := 
     #AUTH_USERS = set(int(x) for x in (id1, id2)) 👈 Type exactly as that and replace id1 and id2 with the id's of the telegram users, who you want to allow for multitasking. You cand add many users like that!
+    
     DOWNLOAD_LOCATION = "./DOWNLOADS" # The download location for users. (Don't change anything in this field!)
     ADMIN_LOCATION = "./ADOWNLOADS" # The download location for auth users. (Don't change anything in this field!)
+    CREDENTIALS_LOCATION = "./CREDENTIALS" # Location where your mega.nz credentials for megatools gets saved if you provide them. (Don't change anything in this field!)
